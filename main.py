01f9da9792a519ebff5887ab02b54fef1e1959ee
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-03-22 23:15:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+from pyrogram import Client, filters 
+
+
+API_ID = "11266671"
+API_HASH = "e46ba5c5399e3407a1d973cb93aaeabd"
+BOT_TOKEN = "5559287245:AAGRi6k45DJ_IVuHaCygLnfX9vtpk1qhND4"
+
+
+ARSHAD = Client(
+    name="Pyrogrambot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("I Am STARTING")
+
+
+ARSHAD.run()
+
+
+
+
+
+
+
+
+
+
 
